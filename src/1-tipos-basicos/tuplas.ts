5c77272172b83352b299caf9f69abb0334372eeb
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-05-17 13:48:22 -0300

    Update tuplas.ts

---
+++ src/1-tipos-basicos/tuplas.ts
@@ -0,0 +1,24 @@
+let pets: [string, string, string] = [
+	'Lora',
+	'Thor',
+	'Amendoin'
+];
+
+/* let pets: [string, string] = [
+ 'Lora',
+ 'Thor',
+ 'Amendoin
+]; ERROR, o array "pets" têm que nescessariamente
+ser composto por 2 strings */
+
+const lora: [string, number] = [
+	'Lora', 3
+]
+
+const thor: [string, number] = [
+	'Thor', 1
+]
+
+const amendoin: [string, number] = [
+	'Amendoin', 2
+]
