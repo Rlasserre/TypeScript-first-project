14cd46ed6bf9f2ca0de844d01481a0325ec0229a
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-05-17 14:02:27 -0300

    Update enum.ts

--- src/1-tipos-basicos/enum.ts
+++ src/1-tipos-basicos/enum.ts
@@ -4,8 +4,22 @@ enum permissoes {
     viewer
 }
 
-const usuario = {
-    nivel: permissoes.admin
+let usuario = {
+    nivel: permissoes.viewer
 }
 
 console.log(usuario);
+
+enum acessLevel {
+    admin = 'ADMIN',
+    editor = 'EDITOR',
+    viewer = 'VIEWER'
+}
+
+let user = {
+    acess: acessLevel.editor
+}
+
+console.log(usuario);
+
+
