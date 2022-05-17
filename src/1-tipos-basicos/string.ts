3c5da163d9b6aa76edc30973eaef23b818741738
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-05-17 09:40:02 -0300

    Create string.ts

---
+++ src/1-tipos-basicos/string.ts
@@ -0,0 +1,15 @@
+let nome: string;
+
+
+//....
+
+nome = 'Rafael';
+
+
+const sobrenome = "Lasserre";
+
+function nomeCompleto(nome: string, sobrenome: string) {
+    return `${nome} ${sobrenome}`
+}
+
+nomeCompleto('Rafael', 'lasserre');
