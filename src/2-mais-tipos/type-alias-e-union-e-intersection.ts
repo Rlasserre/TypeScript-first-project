3feeaa2f3eb5a619376203f60af5dd26948d28cf
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-05-19 17:21:22 -0300

    Update type-alias-e-union-e-intersection.ts

--- src/2-mais-tipos/type-alias-e-union-e-intersection.ts
+++ src/2-mais-tipos/type-alias-e-union-e-intersection.ts
@@ -2,5 +2,11 @@ type User = {
     name: String;
     lastName: String;
     DateOfBirth: String;
-    age: Number;
+    age?: Number;
+}
+
+const Rafael: User = {
+    name: 'Rafael',
+    lastName: 'Lassere',
+    DateOfBirth: '01/01/1981'
 }
