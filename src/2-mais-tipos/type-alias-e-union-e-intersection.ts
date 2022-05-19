91f9a70a28b864a1523922a37cea2ec2ef96179a
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-05-19 18:47:03 -0300

    Update type-alias-e-union-e-intersection.ts
    Union types

--- src/2-mais-tipos/type-alias-e-union-e-intersection.ts
+++ src/2-mais-tipos/type-alias-e-union-e-intersection.ts
@@ -11,3 +11,17 @@ const Rafael: User = {
     DateOfBirth: '01/01/1981'
     //a falta de qualquer atributo, sem interrogação dará um erro.
 }
+
+/* Union types tem a função de ||, 
+ele idinca que o type vai ser de um tipo ou de outro. */
+
+type logLevel = 'info' | 'error' | 'debug';
+
+function logMessage(message: string, level: logLevel) {
+    console.log(`[${level} - ${message}]`);
+}
+
+logMessage('Uma mensagem info', 'error');
+logMessage('Uma mensagem info', 'info');
+logMessage('Uma mensagem info', 'debug');
+logMessage('Uma mensagem info', 'aushSANDAJN');
