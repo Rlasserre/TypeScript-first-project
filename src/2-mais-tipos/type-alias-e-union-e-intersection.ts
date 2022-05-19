028647b5389cb54ff5bddf4261239f23e0b3fbd6
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-05-19 17:22:23 -0300

    Update type-alias-e-union-e-intersection.ts

--- src/2-mais-tipos/type-alias-e-union-e-intersection.ts
+++ src/2-mais-tipos/type-alias-e-union-e-intersection.ts
@@ -2,11 +2,12 @@ type User = {
     name: String;
     lastName: String;
     DateOfBirth: String;
-    age?: Number;
+    age?: Number; // a interrogação ? torna o campo opcional.
 }
 
 const Rafael: User = {
     name: 'Rafael',
     lastName: 'Lassere',
     DateOfBirth: '01/01/1981'
+    //a falta de qualquer atributo, sem interrogação dará um erro.
 }
