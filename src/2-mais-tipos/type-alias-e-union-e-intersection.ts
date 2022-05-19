15f5146c69f7701b661e5ffa7a0f9b547deadb6d
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-05-19 18:55:25 -0300

    Update type-alias-e-union-e-intersection.ts

--- src/2-mais-tipos/type-alias-e-union-e-intersection.ts
+++ src/2-mais-tipos/type-alias-e-union-e-intersection.ts
@@ -1,15 +1,15 @@
 type User = {
-    name: String;
-    lastName: String;
-    DateOfBirth: String;
-    age?: Number; // a interrogação ? torna o campo opcional.
+  name: string;
+  lastName: string;
+  DateOfBirth: string;
+  age?: number; // a interrogação ? torna o campo opcional.
 }
 
 const Rafael: User = {
-    name: 'Rafael',
-    lastName: 'Lassere',
-    DateOfBirth: '01/01/1981'
-    //a falta de qualquer atributo, sem interrogação dará um erro.
+  name: 'Rafael',
+  lastName: 'Lassere',
+  DateOfBirth: '01/01/1981'
+  //a falta de qualquer atributo, sem interrogação dará um erro.
 }
 
 /* Union types tem a função de ||, 
@@ -18,10 +18,28 @@ ele idinca que o type vai ser de um tipo ou de outro. */
 type logLevel = 'info' | 'error' | 'debug';
 
 function logMessage(message: string, level: logLevel) {
-    console.log(`[${level} - ${message}]`);
+  console.log(`[${level} - ${message}]`);
 }
 
 logMessage('Uma mensagem info', 'error');
 logMessage('Uma mensagem info', 'info');
 logMessage('Uma mensagem info', 'debug');
-logMessage('Uma mensagem info', 'aushSANDAJN');
+//logMessage('Uma mensagem info', 'aushSANDAJN'); "APRESENTA ERRO"
+
+
+//Intersections type: &.
+
+type About = {
+  bio: string;
+  interests: string[]
+}
+
+type Profile = User & About;
+const userWithprofile: Profile = {
+  name: 'Maraisa',
+  lastName: 'Matos',
+  DateOfBirth: '01/01/1990',
+  age: 19,
+  bio: 'instagram, gestão e midias sociais',
+  interests: ['musica', 'series', 'midias sociais']
+}
